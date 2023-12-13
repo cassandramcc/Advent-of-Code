324f4958bd49cd05677c81c06adbff3c356aaaf1
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2023-12-13 11:25:13 +0000

    day 1 attempt

--- day1/main.go
+++ day1/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 const (
 	inputs = "inputs/"
-	test   = "test.txt"
-	part1  = "part1.txt"
 )
 
 func main() {
-	solve(test)
+	args := os.Args[1:]
+	fmt.Println(solveTwo(args[0]))
 }
 
 func getFileString(file string) string {
@@ -27,7 +28,7 @@ func getFileString(file string) string {
 	return string(data)
 }
 
-func solve(file string) int {
+func solveOne(file string) int {
 	data := getFileString(file)
 	// split the file into lines
 	lines := strings.Split(data, "\n")
@@ -44,6 +45,77 @@ func solve(file string) int {
 			os.Exit(1)
 		}
 		total += fullNumber
+	}
+	return total
+}
+
+func convertDigitString(digit string) int {
+	digitStrings := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	// Try to convert the digit to an int
+	// If it fails, the digit is text based and needs to be converted via an index in the digitStrings slice
+	i, err := strconv.Atoi(digit)
+	if err != nil {
+		i = slices.Index(digitStrings, digit) + 1
+	}
+	return i
+}
+
+func findDigits(line string) int {
+	digitStrings := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	firstIndex := math.MaxInt
+	lastIndex := -math.MaxInt
+
+	var firstDigit string
+	var lastDigit string
+
+	// Find the first and last string based digit
+	for _, digit := range digitStrings {
+		firstIdx := strings.Index(line, digit)
+		lastIdx := strings.LastIndex(line, digit)
+		if firstIdx != -1 && firstIdx < firstIndex {
+			firstIndex = firstIdx
+			firstDigit = digit
+		}
+
+		if lastIdx != -1 && lastIdx > lastIndex {
+			lastIndex = lastIdx
+			lastDigit = digit
+		}
+	}
+
+	// Find the int based digits and check if the first one is before the first text based digit,
+	// and if the last one is after the last text based digit
+	digit := regexp.MustCompile(`\d`)
+	digits := digit.FindAllString(line, -1)
+
+	if len(digits) >= 1 {
+		if strings.Index(line, digits[0]) < firstIndex {
+			firstDigit = digits[0]
+		}
+
+		if strings.Index(line, digits[len(digits)-1]) > lastIndex {
+			lastDigit = digits[len(digits)-1]
+		}
+	}
+
+	firstInt := convertDigitString(firstDigit)
+	lastInt := convertDigitString(lastDigit)
+
+	return firstInt*10 + lastInt
+}
+
+func solveTwo(file string) int {
+	data := getFileString(file)
+
+	// split the file into lines
+	lines := strings.Split(data, "\n")
+
+	var total int
+
+	for _, line := range lines {
+		total += findDigits(line)
 	}
 	return total
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -6,9 +6,77 @@ import (
 )
 
 func TestGetFileString(t *testing.T) {
-	assert.Equal(t, "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", getFileString("test.txt"))
+	assert.Equal(t, "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", getFileString("test1.txt"))
 }
 
-func TestSolve(t *testing.T) {
-	assert.Equal(t, 142, solve("test.txt"))
+func TestSolveOne(t *testing.T) {
+	assert.Equal(t, 142, solveOne("test1.txt"))
+}
+
+func TestSolveTwo(t *testing.T) {
+	assert.Equal(t, 281, solveTwo("test2.txt"))
+}
+
+func TestConvertDigitString(t *testing.T) {
+	tt := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+	}
+
+	for tc, exp := range tt {
+		t.Run(tc, func(t *testing.T) {
+			assert.Equal(t, exp, convertDigitString(tc))
+		})
+	}
+}
+
+func TestFindDigits(t *testing.T) {
+	tt := map[string]int{
+		"two1nine":                               29,
+		"4nineeightseven2":                       42,
+		"fivethreeonezblqnsfk1":                  51,
+		"four156":                                46,
+		"3qcf":                                   33,
+		"6gd9":                                   69,
+		"z5":                                     55,
+		"3five5":                                 35,
+		"499jvmmrfzkmbppzcm":                     49,
+		"66":                                     66,
+		"four1dcczj":                             41,
+		"1rdtwofjvdllht5eightsixfourbl":          14,
+		"3four9":                                 39,
+		"zgnoneightseveneightseven5d2fivefourjp": 14,
+		"fouronetwo7seventwoeight":               48,
+		"2lssdgdvhl":                             22,
+		"zjlgvvpzpone9":                          19,
+		"doneightn":                              18,
+		"zoneight234":                            14,
+		"oneightone":                             11,
+	}
+
+	for tc, exp := range tt {
+		t.Run(tc, func(t *testing.T) {
+			assert.Equal(t, exp, findDigits(tc))
+		})
+	}
+}
+
+func TestOverlaps(t *testing.T) {
+	assert.Equal(t, 99, findDigits("nine7777nine"))
 }
