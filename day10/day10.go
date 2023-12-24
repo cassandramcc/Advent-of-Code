f5001f64358bcdbd6f75dcef51ce369880fe7461
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2023-12-24 22:23:56 +0000

    day 10 1

--- day10/day10.go
+++ day10/day10.go
@@ -2,7 +2,6 @@ package day10
 
 import (
 	"advent-of-code/common"
-	"fmt"
 	"strings"
 )
 
@@ -10,19 +9,15 @@ type pipe struct {
 	pipeType string
 	x        int
 	y        int
-	input    *pipe
-	output   *pipe
 }
 
 func SolveOne(file string) int {
 	lines := common.GetFileLines(file)
 	var grid [][]string
 	sPipe := pipe{
-		pipeType: "",
+		pipeType: "S",
 		x:        0,
 		y:        0,
-		input:    nil,
-		output:   nil,
 	}
 	for i, line := range lines {
 		if strings.Contains(line, "S") {
@@ -32,39 +27,135 @@ func SolveOne(file string) int {
 		grid = append(grid, strings.Split(line, ""))
 	}
 
+	var visitedPipes []pipe
+	visitedPipes = append(visitedPipes, sPipe)
 	pipes := findSPipeAdjacents(sPipe, grid)
-	fmt.Println(pipes)
-
-	return 0
+	endPipe := pipes[1]
+	currentPipe := pipes[0]
+	for !isEndPipe(currentPipe, endPipe) {
+		visitedPipes = append(visitedPipes, currentPipe)
+		nextPipes := findNextPipe(currentPipe, grid)
+		if !alreadyVisitedPipe(nextPipes[0], visitedPipes) {
+			currentPipe = nextPipes[0]
+		} else {
+			currentPipe = nextPipes[1]
+		}
+	}
+	return (len(visitedPipes) + 1) / 2
 }
 
 func SolveTwo(file string) int {
-	//data := common.GetFileText(file)
 	return 0
 }
 
-func isValidPipeChar(s string) bool {
-	return s != "."
+func isEndPipe(p pipe, endPipe pipe) bool {
+	return p.x == endPipe.x && p.y == endPipe.y
+}
+func alreadyVisitedPipe(p pipe, visted []pipe) bool {
+	for _, v := range visted {
+		if v.x == p.x && v.y == p.y {
+			return true
+		}
+	}
+	return false
+}
+
+func findNextPipe(p pipe, grid [][]string) []pipe {
+	if p.pipeType == "7" {
+		return []pipe{
+			{
+				pipeType: grid[p.y+1][p.x],
+				x:        p.x,
+				y:        p.y + 1,
+			},
+			{
+				pipeType: grid[p.y][p.x-1],
+				x:        p.x - 1,
+				y:        p.y,
+			},
+		}
+	} else if p.pipeType == "-" {
+		return []pipe{
+			{
+				pipeType: grid[p.y][p.x-1],
+				x:        p.x - 1,
+				y:        p.y,
+			},
+			{
+				pipeType: grid[p.y][p.x+1],
+				x:        p.x + 1,
+				y:        p.y,
+			},
+		}
+	} else if p.pipeType == "L" {
+		return []pipe{
+			{
+				pipeType: grid[p.y][p.x+1],
+				x:        p.x + 1,
+				y:        p.y,
+			},
+			{
+				pipeType: grid[p.y-1][p.x],
+				x:        p.x,
+				y:        p.y - 1,
+			},
+		}
+	} else if p.pipeType == "J" {
+		return []pipe{
+			{
+				pipeType: grid[p.y][p.x-1],
+				x:        p.x - 1,
+				y:        p.y,
+			},
+			{
+				pipeType: grid[p.y-1][p.x],
+				x:        p.x,
+				y:        p.y - 1,
+			},
+		}
+	} else if p.pipeType == "F" {
+		return []pipe{
+			{
+				pipeType: grid[p.y][p.x+1],
+				x:        p.x + 1,
+				y:        p.y,
+			},
+			{
+				pipeType: grid[p.y+1][p.x],
+				x:        p.x,
+				y:        p.y + 1,
+			},
+		}
+	} else if p.pipeType == "|" {
+		return []pipe{
+			{
+				pipeType: grid[p.y-1][p.x],
+				x:        p.x,
+				y:        p.y - 1,
+			},
+			{
+				pipeType: grid[p.y+1][p.x],
+				x:        p.x,
+				y:        p.y + 1,
+			},
+		}
+	}
+	return nil
 }
 
 func findSPipeAdjacents(sPipe pipe, grid [][]string) []pipe {
+	// right and down
 	if sPipe.x+1 <= len(grid[0]) && sPipe.y+1 < len(grid) && (grid[sPipe.y][sPipe.x+1] == "7" || grid[sPipe.y][sPipe.x+1] == "-" || grid[sPipe.y][sPipe.x+1] == "J") && (grid[sPipe.y+1][sPipe.x] == "|" || grid[sPipe.y+1][sPipe.x] == "L" || grid[sPipe.y+1][sPipe.x] == "J") {
 
 		pipes := []pipe{{
 			pipeType: grid[sPipe.y][sPipe.x+1],
 			x:        sPipe.x + 1,
 			y:        sPipe.y,
-			input:    &sPipe,
-			output:   nil,
 		}, {
 			pipeType: grid[sPipe.y+1][sPipe.x],
 			x:        sPipe.x,
-			y:        sPipe.y,
-			input:    nil,
-			output:   &sPipe,
+			y:        sPipe.y + 1,
 		}}
-		sPipe.output = &pipes[0]
-		sPipe.input = &pipes[1]
 		return pipes
 	}
 
@@ -72,19 +163,13 @@ func findSPipeAdjacents(sPipe pipe, grid [][]string) []pipe {
 		pipes := []pipe{{
 			pipeType: grid[sPipe.y+1][sPipe.x],
 			x:        sPipe.x,
-			y:        sPipe.y - 1,
-			input:    &sPipe,
-			output:   nil,
+			y:        sPipe.y + 1,
 		},
 			{
 				pipeType: grid[sPipe.y][sPipe.x-1],
 				x:        sPipe.x - 1,
 				y:        sPipe.y,
-				input:    nil,
-				output:   &sPipe,
 			}}
-		sPipe.output = &pipes[0]
-		sPipe.input = &pipes[1]
 		return pipes
 	}
 
@@ -93,17 +178,11 @@ func findSPipeAdjacents(sPipe pipe, grid [][]string) []pipe {
 			pipeType: grid[sPipe.y][sPipe.x-1],
 			x:        sPipe.x - 1,
 			y:        sPipe.y,
-			input:    &sPipe,
-			output:   nil,
 		}, {
 			pipeType: grid[sPipe.y-1][sPipe.x],
 			x:        sPipe.x,
-			y:        sPipe.y + 1,
-			input:    nil,
-			output:   &sPipe,
+			y:        sPipe.y - 1,
 		}}
-		sPipe.output = &pipes[0]
-		sPipe.input = &pipes[1]
 		return pipes
 	}
 
@@ -111,18 +190,12 @@ func findSPipeAdjacents(sPipe pipe, grid [][]string) []pipe {
 		pipes := []pipe{{
 			pipeType: grid[sPipe.y-1][sPipe.x],
 			x:        sPipe.x,
-			y:        sPipe.y + 1,
-			input:    &sPipe,
-			output:   nil,
+			y:        sPipe.y - 1,
 		}, {
 			pipeType: grid[sPipe.y][sPipe.x+1],
 			x:        sPipe.x + 1,
 			y:        sPipe.y,
-			input:    nil,
-			output:   &sPipe,
 		}}
-		sPipe.output = &pipes[0]
-		sPipe.input = &pipes[1]
 		return pipes
 	}
 	return nil
