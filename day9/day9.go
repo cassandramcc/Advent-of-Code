a6a75a018d61b1a5e513ac35da406ffaaf3870c5
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2023-12-24 16:58:49 +0000

    day 9 part 2

--- day9/day9.go
+++ day9/day9.go
@@ -2,6 +2,7 @@ package day9
 
 import (
 	"advent-of-code/common"
+	"slices"
 	"strings"
 )
 
@@ -14,34 +15,44 @@ func SolveOne(file string) int {
 	}
 
 	var total int
-
 	for _, sequence := range sequences {
-		total += extrapolate(sequence)
+		total += extrapolateForwards(sequence)
 	}
 	return total
 }
 
 func SolveTwo(file string) int {
-	return 0
+	lines := common.GetFileLines(file)
+	var sequences [][]int
+	for _, line := range lines {
+		s := strings.Split(line, " ")
+		sequences = append(sequences, common.ConvertToIntSlice(s))
+	}
+
+	var total int
+	for _, sequence := range sequences {
+		total += extrapolateBackwards(sequence)
+	}
+	return total
 }
 
 // The next value is the sum of last item in the slices of differences
-func extrapolate(sequence []int) int {
+func extrapolateForwards(sequence []int) int {
 	differences := findDifferences(sequence)
 
 	var nextValue int
 	for _, difference := range differences {
-		nextValue += difference
+		nextValue += difference[len(difference)-1]
 	}
 	return nextValue + sequence[len(sequence)-1]
 }
 
-func findDifferences(sequence []int) []int {
-	var differences []int
+func findDifferences(sequence []int) [][]int {
+	var differences [][]int
 	currentSequence := sequence
 	for !isBaseDifference(currentSequence) {
 		nextSequence := findDifference(currentSequence)
-		differences = append(differences, nextSequence[len(nextSequence)-1])
+		differences = append(differences, nextSequence)
 		currentSequence = nextSequence
 	}
 	return differences
@@ -57,4 +68,14 @@ func findDifference(numbers []int) []int {
 		differences = append(differences, numbers[i+1]-numbers[i])
 	}
 	return differences
+}
+
+func extrapolateBackwards(sequence []int) int {
+	differences := findDifferences(sequence)
+	slices.Reverse(differences)
+	prevValue := differences[0][0]
+	for _, difference := range differences[1:] {
+		prevValue = difference[0] - prevValue
+	}
+	return sequence[0] - prevValue
 }
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -8,3 +8,7 @@ import (
 func TestSolveOne(t *testing.T) {
 	assert.Equal(t, 114, SolveOne("inputs/test.txt"))
 }
+
+func TestSolveTwo(t *testing.T) {
+	assert.Equal(t, 2, SolveTwo("inputs/test.txt"))
+}
