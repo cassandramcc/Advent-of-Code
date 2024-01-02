00a48d2fea4a80b1d24170912ae1dbc40c9cddbe
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2024-01-02 09:18:58 +0000

    day10 attempt

--- day10/day10.go
+++ day10/day10.go
@@ -2,6 +2,8 @@ package day10
 
 import (
 	"advent-of-code/common"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -45,7 +47,47 @@ func SolveOne(file string) int {
 }
 
 func SolveTwo(file string) int {
-	return 0
+	lines := common.GetFileLines(file)
+	var grid [][]string
+	sPipe := pipe{
+		pipeType: "S",
+		x:        0,
+		y:        0,
+	}
+	for i, line := range lines {
+		if strings.Contains(line, "S") {
+			sPipe.x = strings.Index(line, "S")
+			sPipe.y = i
+		}
+		grid = append(grid, strings.Split(line, ""))
+	}
+
+	var visitedPipes []pipe
+	visitedPipes = append(visitedPipes, sPipe)
+	pipes := findSPipeAdjacents(sPipe, grid)
+	endPipe := pipes[1]
+	currentPipe := pipes[0]
+	for !isEndPipe(currentPipe, endPipe) {
+		visitedPipes = append(visitedPipes, currentPipe)
+		nextPipes := findNextPipe(currentPipe, grid)
+		if !alreadyVisitedPipe(nextPipes[0], visitedPipes) {
+			currentPipe = nextPipes[0]
+		} else {
+			currentPipe = nextPipes[1]
+		}
+	}
+	visitedPipes = append(visitedPipes, endPipe)
+	nonLoop := findNonLoop(visitedPipes, grid)
+
+	var count int
+	for _, n := range nonLoop {
+		if isInLoop(*n, visitedPipes, len(grid), len(grid[0])) {
+			count++
+			n.pipeType = "I"
+		}
+	}
+	printGrid(visitedPipes, nonLoop, []int{len(grid), len(grid[0])})
+	return count
 }
 
 func isEndPipe(p pipe, endPipe pipe) bool {
@@ -58,6 +100,51 @@ func alreadyVisitedPipe(p pipe, visted []pipe) bool {
 		}
 	}
 	return false
+}
+
+func printGrid(pipes []pipe, nonPipe []*pipe, size []int) {
+	colorRed := "\033[0;31m"
+	colorNone := "\033[0m"
+	colorBlue := "\033[0;34m"
+
+	var grid [][]string
+	for i := 0; i < size[0]; i++ {
+		var row []string
+		for j := 0; j < size[1]; j++ {
+			row = append(row, ".")
+		}
+		grid = append(grid, row)
+	}
+
+	for _, p := range pipes {
+		grid[p.y][p.x] = p.pipeType
+	}
+
+	for _, p := range nonPipe {
+		grid[p.y][p.x] = p.pipeType
+	}
+
+	for y, g := range grid {
+		for x, v := range g {
+			if v == "I" {
+				fmt.Fprintf(os.Stdout, "%s%s%s", colorRed, v, colorNone)
+			} else if alreadyVisitedPipe(pipe{x: x, y: y}, pipes) {
+				fmt.Fprintf(os.Stdout, "%s%s%s", colorBlue, v, colorNone)
+			} else {
+				fmt.Printf(v)
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func findPipeAtLoc(p pipe, pipes []pipe) pipe {
+	for _, v := range pipes {
+		if v.x == p.x && v.y == p.y {
+			return v
+		}
+	}
+	return pipe{}
 }
 
 func findNextPipe(p pipe, grid [][]string) []pipe {
@@ -199,4 +286,95 @@ func findSPipeAdjacents(sPipe pipe, grid [][]string) []pipe {
 		return pipes
 	}
 	return nil
+}
+
+func findNonLoop(pipes []pipe, grid [][]string) []*pipe {
+	var nonPipes []*pipe
+	for i := range grid {
+		for j := range grid[i] {
+			p := pipe{pipeType: grid[i][j], x: j, y: i}
+			if !alreadyVisitedPipe(p, pipes) {
+				nonPipes = append(nonPipes, &p)
+			}
+		}
+	}
+	return nonPipes
+}
+
+func noStraights(m map[string]int) bool {
+	_, ok := m["-"]
+	return !ok
+}
+
+// This is the problem
+// this is an attempt at the point in polygon problem with discrete ray casting
+// however as it is discrete, it can't deal with when a point is inside the loop
+// but on the vertical direction, there is no - pipe, only | type pipes.
+// so if a point is inline with these vertical pipes, how to deal with it?
+
+// if any of the counts are even then it's outside
+func isInLoop(p pipe, loop []pipe, bottom, side int) bool {
+
+	var verticalUpRay []string
+	for y := p.y; y <= bottom; y++ {
+		foundPipe := findPipeAtLoc(pipe{x: p.x, y: y}, loop)
+		if alreadyVisitedPipe(pipe{x: p.x, y: y}, loop) {
+			verticalUpRay = append(verticalUpRay, foundPipe.pipeType)
+		}
+	}
+
+	instances := common.CountInstances(verticalUpRay)
+	if !noStraights(instances) {
+		return instances["-"]%2 != 0
+	} else {
+		total := 0
+		for _, v := range instances {
+			total += v
+		}
+		if total != 0 {
+			return (total/2)%2 != 0
+		}
+		return false
+	}
+
+	var verticalDownRay []string
+	for y := p.y; y >= 0; y-- {
+		foundPipe := findPipeAtLoc(pipe{x: p.x, y: y}, loop)
+		if alreadyVisitedPipe(pipe{x: p.x, y: y}, loop) {
+			verticalDownRay = append(verticalDownRay, foundPipe.pipeType)
+		}
+	}
+
+	instances = common.CountInstances(verticalDownRay)
+	if !noStraights(instances) {
+		return instances["-"]%2 != 0
+	}
+
+	var horizontalRightRay []string
+	for x := p.x; x <= side; x++ {
+		foundPipe := findPipeAtLoc(pipe{x: x, y: p.y}, loop)
+		if alreadyVisitedPipe(pipe{x: x, y: p.y}, loop) {
+			horizontalRightRay = append(horizontalRightRay, foundPipe.pipeType)
+		}
+	}
+
+	instances = common.CountInstances(horizontalRightRay)
+	if !noStraights(instances) {
+		return instances["|"]%2 != 0
+	}
+
+	var horizontalLeftRay []string
+	for x := p.x; x >= 0; x-- {
+		foundPipe := findPipeAtLoc(pipe{x: x, y: p.y}, loop)
+		if alreadyVisitedPipe(pipe{x: x, y: p.y}, loop) {
+			horizontalLeftRay = append(horizontalLeftRay, foundPipe.pipeType)
+		}
+	}
+
+	instances = common.CountInstances(horizontalLeftRay)
+	if !noStraights(instances) {
+		return instances["|"]%2 != 0
+	}
+
+	return false
 }
--- day10/day10_test.go
+++ day10/day10_test.go
@@ -12,3 +12,27 @@ func TestSolveOneTestOne(t *testing.T) {
 func TestSolveOneTestTwo(t *testing.T) {
 	assert.Equal(t, 8, SolveOne("inputs/test2.txt"))
 }
+
+func TestSolveTwoTestOne(t *testing.T) {
+	assert.Equal(t, 1, SolveTwo("inputs/test1.txt"))
+}
+
+func TestSolveTwoTestThree(t *testing.T) {
+	assert.Equal(t, 4, SolveTwo("inputs/test3.txt"))
+}
+
+func TestSolveTwoTestFour(t *testing.T) {
+	assert.Equal(t, 4, SolveTwo("inputs/test4.txt"))
+}
+
+func TestSolveTwoTestFive(t *testing.T) {
+	assert.Equal(t, 8, SolveTwo("inputs/test5.txt"))
+}
+
+func TestSolveTwoTestSix(t *testing.T) {
+	assert.Equal(t, 10, SolveTwo("inputs/test6.txt"))
+}
+
+func TestSolveTwoTestSeven(t *testing.T) {
+	assert.Equal(t, 1, SolveTwo("inputs/test7.txt"))
+}
